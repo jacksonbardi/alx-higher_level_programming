e6bfbaa9783b5c0c8b051aba80a226460b8bf39a
jacksonbardi <rachadwafir8@gmail.com>
2024-01-04 07:11:15 -0800

    Update

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This module contains a class Rectangle
+
+>>> Rectangle = __import__('0-rectangle').Rectangle
+
+>>> my_rectangle = Rectangle()
+>>> print(type(my_rectangle))
+<class '0-rectangle.Rectangle'>
+
+>>> print(my_rectangle.__dict__)
+{}
+
+"""
+
+
+class Rectangle:
+    """This is an empty class"""
+    pass
